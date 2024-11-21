ff5b981c8b67feaf770d53d96ad671899c4b2603
Abdelrhman-Ellithy <abdelarhmanellithy@gmail.com>
2024-11-21 14:07:05 +0200

    Add the project

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -2,8 +2,8 @@ package Runner;
 import Ellithium.core.base.BDDSetup;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-        glue = {"stepDefinitions","Base"}, // path to your stepDefinitions package, note you should use . instead of /
-        features="src/main/resources/features" // path to your features folder
-)
+        glue = {"stepDefinitions","Base"}, 
+        features="src/main/resources/features"
+)    // All the methods, packages used I created in the framework that is used
 public class TestRunner extends BDDSetup {
 }
